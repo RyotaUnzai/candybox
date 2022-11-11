33725c22c24e4aa172f0e017239859398e8a492c
UnPySide <satellitear@gmail.com>
2022-11-11 22:17:58 +0900

    added QtCustom Widgets

--- src/views/home/home.py
+++ src/views/home/home.py
@@ -1,8 +1,9 @@
 import os
 import core
+from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
-from PySide2.QtUiTools import QUiLoader
+
 
 import QtCustom
 
@@ -12,47 +13,29 @@ class homeWidget(QWidget):
     def __init__(self, parent=None, *args, **kwargs) -> None:
 
         super(homeWidget, self).__init__(parent, *args, **kwargs)
-        uiLoader = QUiLoader()
+        uiLoader = QtCustom.QExUiLoader()
         __uiFilePath = os.path.join(core.PATH_VIEWS, "home", "home.ui")
         ui = uiLoader.load(__uiFilePath)
         self.ui = ui
         self.ui.setParent(parent)
         self.setObjectName("Home")
-        self.createQAnimationComboBox()
-        self.changeWidget()
-        # self.ui.Cb_qss.deleteLater()
 
-    def createQAnimationComboBox(self):
-        self.ComboBox = QtCustom.QAnimationComboBox(self.ui)
         self.ComboBox_ListView = QListView(self.ui)
         self.ComboBox_ListView.setObjectName("Cb_qss")
         self.ComboBox_ListView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.ComboBox.setView(self.ComboBox_ListView)
         self.ComboBox_LineEdit = QLineEdit(self.ui)
         self.ComboBox_LineEdit.setObjectName("Cb_qss")
         self.ComboBox_LineEdit.setReadOnly(True)
-        self.ComboBox.setLineEdit(self.ComboBox_LineEdit)
-        self.ComboBox.setObjectName("Cb_qss")
-        self.ComboBox.PopupOffcet = (0, 10)
-        self.ComboBox.fade = True
-        self.ComboBox.slide = False
-        self.ComboBox.stretch = True
+
+        self.Cb_qss.setView(self.ComboBox_ListView)
+        self.Cb_qss.setLineEdit(self.ComboBox_LineEdit)
+        self.Cb_qss.PopupOffcet = (0, 10)
+        self.Cb_qss.fade = True
+        self.Cb_qss.slide = False
+        self.Cb_qss.stretch = True
         for i in range(5):
-            self.ComboBox.addItem("Item %s" % i)
+            self.Cb_qss.addItem("Item %s" % i)
 
-    def changeWidget(self):
-        for num in range(self.ui.gridLayout.count()):
-            item = self.ui.gridLayout.itemAt(num)
-            try:
-                widget = item.widget()
-                objectName = widget.objectName()
-                if objectName == "Cb_qss":
-                    pos = self.ui.gridLayout.getItemPosition(num)
-            except BaseException:
-                pass
-
-        self.ui.Cb_qss.deleteLater()
-        self.ui.Cb_qss = self.ComboBox
-        self.ui.gridLayout.addWidget(
-            self.ui.Cb_qss, pos[0], pos[1], pos[2], pos[3]
-        )
+    @property
+    def Cb_qss(self):
+        return self.ui.Cb_qss
--- src/views/schedule/schedule.py
+++ src/views/schedule/schedule.py
@@ -1,7 +1,8 @@
 import os
 import core
-from PySide2.QtWidgets import QWidget
-from PySide2.QtUiTools import loadUiType, QUiLoader
+from PySide2.QtWidgets import QWidget, QPushButton
+
+import QtCustom
 
 
 class scheduleWidget(QWidget):
@@ -9,9 +10,30 @@ class scheduleWidget(QWidget):
     def __init__(self, parent=None, *args, **kwargs) -> None:
 
         super(scheduleWidget, self).__init__(parent, *args, **kwargs)
-        uiLoader = QUiLoader()
+        uiLoader = QtCustom.QExUiLoader()
         __uiFilePath = os.path.join(core.PATH_VIEWS, "schedule", "schedule.ui")
         ui = uiLoader.load(__uiFilePath)
         self.ui = ui
         self.ui.setParent(parent)
         self.setObjectName("Schedule")
+
+        self.Cb_qss.PopupOffcet = (0, 10)
+        self.Cb_qss.fade = True
+        self.Cb_qss.slide = True
+        self.Cb_qss.stretch = False
+        for i in range(5):
+            self.Cb_qss.addItem("Item %s" % i)
+
+        for i in range(30):
+            btn = QPushButton("%s" % i)
+            self.FlowLayout.addWidget(btn)
+
+        self.FlowLayout.space = (50, 10)
+
+    @property
+    def Cb_qss(self):
+        return self.ui.Cb_qss
+
+    @property
+    def FlowLayout(self) -> QtCustom.QFlowLayout:
+        return self.ui.QFlowLayout_FlowLayout
